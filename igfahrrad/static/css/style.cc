5c701f8d274fba7d6384906f9b8c50c206ea557c
floabs <florian.aberger@gmail.com>
2024-07-14 13:37:24 +0200

    made the footer dynamically  pinned to the bottom by implementing a style.css aswell as referencing it in baseof.html

---
+++ igfahrrad/static/css/style.cc
@@ -0,0 +1,22 @@
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+#content {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
+
+footer {
+  background-color: #000;
+  color: #fff;
+  text-align: center;
+  padding: 1em 0;
+}
+
