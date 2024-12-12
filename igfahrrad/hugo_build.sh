82af35e7a59629b0034a1097a469f24c87c32d26
caberger <c.aberger@htl-leonding.ac.at>
2024-12-12 19:26:04 +0100

    remove alex

--- igfahrrad/hugo_build.sh
+++ igfahrrad/hugo_build.sh
@@ -1,5 +1,6 @@
-#!/bin/env bash
+#!/usr/bin/env bash
+set -e
 hugo --verbose --minify --destination www --environment production --baseURL https://www.ig-fahrrad.at/
+cp ./.htaccess _index.html www/
 tar -czf www.tgz www
 scp www.tgz igfahrrad:
-
